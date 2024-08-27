17b01d737ae52e7bb5b2ca9cb6325af491dcffff
kauê <156674259+yyukim0@users.noreply.github.com>
2024-08-26 22:34:45 -0300

    Add files via upload

---
+++ aula.js
@@ -0,0 +1,9 @@
+botao = document.querySelector("#b1");
+
+botao.onclick () = () => {
+    if(body.style.backgroundColor == "aqua") {
+        body.style.backgroundColor == "black";
+    } else {
+        body.style.backgroundColor == "aqua";
+    }
+}
